01bae7712e79c90304af0fd01bae96758fbafa19
Sopee Thong <tsopee@gmail.com>
2021-04-29 00:58:21 -0400

    created post and delete routes

---
+++ controllers/add_property.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Property } = require('../../models');
+
+//route to create a new property
+router.post('/', async (req, res) => {
+  try {
+    const newProperty = await Property.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProperty);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//route to delete a property by id
+router.delete('/:id', async (req, res) => {
+    try {
+      const propertyData = await Property.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!propertyData) {
+        res.status(404).json({ message: 'No property found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(propertyData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+module.exports = router;
