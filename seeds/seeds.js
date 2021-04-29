0bf08c12112f7521382a13e40f6d1e7a60f481bc
Sopee Thong <tsopee@gmail.com>
2021-04-29 11:29:19 -0400

    removing bad code

---
+++ seeds/seeds.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/connection');
+const seedUsers = require('./users');
+const seedProperties = require('./properties');
+const seedTenants = require('./tenants');
+
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUsers();
+
+  await seedProperties();
+
+  await seedTenants();
+
+  process.exit(0);
+};
+
+seedAll();
