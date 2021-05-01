924671ff37b3043d149c71cb3a0b85e76910e3d5
Farrancampbell <farranerinandcrew@gmail.com>
2021-05-01 14:44:58 -0400

    Made a few fixes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router(); //Router object 
-
+const homeRoutes = require('./homeRoutes')
 const apiRoutes = require('./api');
 const propertyRoutes = require('./propertyRoutes');
 const tenantRoutes = require('./tenantRoutes');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/property', propertyRoutes);
 router.use('/tenant', tenantRoutes);
 router.use('/api', apiRoutes);
