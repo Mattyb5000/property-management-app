e8f892cc350001d698573d0f570bfec5a1a2be36
Sopee Thong <tsopee@gmail.com>
2021-05-02 13:35:11 -0400

    reverting

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers'); 
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 const { access } = require('fs');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create({helpers});
-
 // Setting up sessions with cookies-ST
 const sess = {
   secret: 'Super secret secret',
@@ -24,19 +20,14 @@ const sess = {
     db: sequelize
   })
 };
-
 app.use(session(sess));
 app.use(express.static('views/images')); //to access images using handlebars -ST
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes); //triggering the routes with Express-ST
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'+PORT));
 });
