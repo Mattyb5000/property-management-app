a16d2e3a8ca5838b3a5f942af28b6226b8ccfbb8
Sopee Thong <tsopee@gmail.com>
2021-05-02 13:38:38 -0400

    reverting

--- controllers/api/properties.js
+++ controllers/api/properties.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Property } = require('../../models');
 const withAuth = require('../../utils/auth');
-
 //route to get all properties
 // router.get('/', async (req, res) => {
 //     const propertyData = await Property.findAll().catch((err) => { 
@@ -10,7 +9,6 @@ const withAuth = require('../../utils/auth');
 //         const properties = propertyData.map((property) => property.get({ plain: true }));
 //         res.render('all', { properties });
 //       });
-
 //       //route to get one property
 //       router.get('/:id', async (req, res) => {
 //         try{ 
@@ -25,26 +23,21 @@ const withAuth = require('../../utils/auth');
 //               res.status(500).json(err);
 //           };     
 //       });
-    
       // const router = require('express').Router();
 // const { Property } = require('../../models');
-
 //route to create a new property
-
 router.post('/',  async (req, res) => {
   try {
     const newProperty = await Property.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
     res.status(200).json(newProperty);
   } catch (err) {
     res.status(400).json(err);
     res.render('add_property', { property });
   }
 });
-
 //route to delete a property by id
 router.delete('/:id', async (req, res) => {
     try {
@@ -54,16 +47,13 @@ router.delete('/:id', async (req, res) => {
           user_id: req.session.user_id,
         },
       });
-  
       if (!propertyData) {
         res.status(404).json({ message: 'No property found with this id!' });
         return;
       }
-  
       res.status(200).json(propertyData);
     } catch (err) {
       res.status(500).json(err);
     }
   });
-
     module.exports = router;
