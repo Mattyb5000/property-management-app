66ed27e17d304ebc2a770666ab5098c9e22e44eb
Liz Emerson <uknity@gmail.com>
2021-05-02 19:49:48 -0400

    property routes

--- controllers/propertyRoutes.js
+++ controllers/propertyRoutes.js
@@ -3,69 +3,70 @@ const { Property, User } = require('../models');
 // const withAuth = require('../utils/auth');
 //property_dashboard route--displays all properties
 
+//get all properties
 router.get('/', async (req, res) => {
-    const propertyData = await Property.findAll({
+  const propertyData = await Property.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['id'],
+      },
+    ],
+  });
+  const properties = propertyData.map((properties) =>
+    properties.get({ plain: true })
+  );
+  res.render('property_dashboard', { properties });
+});
+
+
+// render add property page
+router.get('/update', (req, res) => {
+  res.render('add_property');
+});
+
+
+// route to view a single property on display_property
+router.get('/:id', async (req, res) => {
+  try {
+    const propertyData = await Property.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: [
-            'id'
-          ],
+          attributes: ['id'],
         },
       ],
-
-    });
-        const properties = propertyData.map((properties) => properties.get({ plain: true }));
-        console.log("YOURE ON THE RIGHT TRACK SOP!", properties);
-        res.render('property_dashboard', { properties });
-  
-      });
-
-    // route to view a single property on display_property
-    router.get('/:id', async (req, res) => {
-      try {
-        const propertyData = await Property.findByPk(req.params.id, {
-          include: [
-            {
-              model: User,
-              attributes: [ 'id' ],
-            },
-          ],
-        });
-    
-        const property = propertyData.get({ plain: true });
-        console.log ("WE REAAAAAAAAAAAAAAAADY!!!!!", property);
-        res.render('display_property',  {
-          property });
-      } catch (err) {
-        console.log(err);
-       
-        res.status(500).json(err);
-      }
     });
 
-    // render add property page
-router.get('/add', (req, res) => {
-  res.render('add_property');
+    const property = propertyData.get({ plain: true });
+    console.log('WE REAAAAAAAAAAAAAAAADY!!!!!', property);
+    res.render('display_property', {
+      property,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json(err);
+  }
 });
-      // router.get('/property', withAuth, async (req, res) => {
-      //   try {
-      //     // Find the logged in user based on the session ID
-      //     const userData = await User.findByPk(req.session.user_id, {
-      //       attributes: { exclude: ['password'] },
-      //       include: [{ model: Property }],
-      //     });
-      
-      //     const user = userData.get({ plain: true });
-      
-      //     res.render('property', {
-      //       ...user,
-      //       logged_in: true
-      //     });
-      //   } catch (err) {
-      //     res.status(500).json(err);
-      //   }
-      // });
 
-    
-      module.exports = router;
+// router.get('/property', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Property }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('property', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
